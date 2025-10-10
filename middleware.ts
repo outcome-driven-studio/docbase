58c412cdf5a71689b7853f0731d6355361665526
anianroid <anianroid@gmail.com>
2025-10-10 16:55:12 -0700

    Fix middleware for Vercel edge runtime compatibility

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,12 @@ import { NextResponse, type NextRequest } from "next/server"
 import { createServerClient, type CookieOptions } from "@supabase/ssr"
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next()
+  let res = NextResponse.next({
+    request: {
+      headers: req.headers,
+    },
+  })
+
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -12,14 +17,44 @@ export async function middleware(req: NextRequest) {
           return req.cookies.get(name)?.value
         },
         set(name: string, value: string, options: CookieOptions) {
-          res.cookies.set({ name, value, ...options })
+          req.cookies.set({
+            name,
+            value,
+            ...options,
+          })
+          res = NextResponse.next({
+            request: {
+              headers: req.headers,
+            },
+          })
+          res.cookies.set({
+            name,
+            value,
+            ...options,
+          })
         },
         remove(name: string, options: CookieOptions) {
-          res.cookies.set({ name, value: "", ...options })
+          req.cookies.set({
+            name,
+            value: "",
+            ...options,
+          })
+          res = NextResponse.next({
+            request: {
+              headers: req.headers,
+            },
+          })
+          res.cookies.set({
+            name,
+            value: "",
+            ...options,
+          })
         },
       },
     }
   )
+
   await supabase.auth.getSession()
+
   return res
 }
